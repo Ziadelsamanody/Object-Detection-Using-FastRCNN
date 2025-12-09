fc56ea8a438bb17cf286a2eccd7333db1dd612a5
Ziadelsamanody <ziadelsamanody@gmail.com>
2025-12-09 13:50:19 +0200

    add dataset loader

---
+++ dataset.py
@@ -0,0 +1,69 @@
+import torch
+from torch.utils.data import Dataset 
+from torchvision.datasets import VOCDetection
+
+def voc_to_fast_rcnn(target, label_map):
+    objs = target['annotation']['object']
+    if not isinstance(objs, list): 
+        objs = [objs]
+    
+    boxes = []
+    labels = []
+    for obj in objs:
+        bbox = obj['bndbox']
+        xmin = float(bbox['xmin'])
+        ymin = float(bbox['ymin'])
+        xmax = float(bbox['xmax'])
+        ymax = float(bbox['ymax'])
+
+        boxes.append([xmin, ymin, xmax, ymax])
+        labels.append(label_map[obj['name']])
+    return {
+        'boxes' : torch.tensor(boxes, dtype=torch.float32),
+        'labels' : torch.tensor(labels, dtype=torch.int64),
+    }
+
+
+VOC_CLASSES = [
+    "aeroplane","bicycle","bird","boat","bottle","bus","car","cat","chair",
+    "cow","diningtable","dog","horse","motorbike","person","pottedplant",
+    "sheep","sofa","train","tvmonitor"
+]
+
+
+label_map = {clss : i + 1 for i, clss in enumerate(VOC_CLASSES)}  # starting from 1 cause 0 for background
+
+
+class VOCFastRCNN(Dataset):
+    def __init__(self, year='2007', split='train'):
+        try:
+            self.dataset = VOCDetection(
+                root='./data',
+                year=year,
+                image_set=split,
+                download=True
+            )
+        except RuntimeError:
+            print("Auto-download failed. Attempting to use existing data...")
+            self.dataset = VOCDetection(
+                root='./data',
+                year=year,
+                image_set=split,
+                download=False
+            )
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        img, target = self.dataset[idx]
+        target = voc_to_fast_rcnn(target, label_map)
+
+        return img, target
+
+
+if __name__ == "__main__":
+    data = VOCFastRCNN(year='2007', split='train')
+    img, target = data[0]
+    print(img)
+    print(target)
